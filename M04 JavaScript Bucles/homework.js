1e879eb71e3c38f27fec8603e64d5d3b47ba3bc8
Ru5095 <juangma95@gmail.com>
2023-07-06 16:26:50 -0400

    Casi terminado

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -163,6 +163,22 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if ((num1 < 0) || (num2 < 0) || (num3 < 0)) {
+      return "Hay negativos"
+   }
+   else if ((num1 > num2) && (num1 > num3) && (num1 > 0)) {
+      return "Numero 1 es mayor y positivo"
+   }
+   else if((num3 > num2) && (num3 > num1)) {
+      num3++
+      return num3
+   }
+   else if((num1 == 0) && (num2 == 0) && (num3 == 0)) {
+      return "Error"
+   }
+   else {
+      return false
+   }
 }
 
 function esPrimo(num) {
@@ -172,18 +188,43 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+   let divisor = 1;
+   let divCount = 0
+   for (num, divisor; divisor <= num; divisor++) {
+      if (num % divisor === 0) {
+         divCount++
+      }
+   }
+   if (divCount === 2) {
+      return true;
+   }
+   else {
+      return false;
+   }
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if (Boolean(valor) == true) {
+      return "Soy verdadero"
+   }
+   else{
+      return "Soy falso"
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if ((num >= 100) && (num <= 999)) {
+      return true
+   }
+   else {
+      return false
+   }
 }
 
 function doWhile(num) {
@@ -191,6 +232,7 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+   do num++
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
